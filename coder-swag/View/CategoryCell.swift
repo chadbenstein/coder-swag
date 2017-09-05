1929551682f6cd6c85821698ebcdd5d4b6da6cbc
Chad Stein <chadstein@Chads-MacBook-Pro.local>
2017-09-05 16:13:18 -0400

    added table view & connected outlets

---
+++ coder-swag/View/CategoryCell.swift
@@ -0,0 +1,28 @@
+//
+//  CategoryCell.swift
+//  coder-swag
+//
+//  Created by Chad Stein on 9/5/17.
+//  Copyright © 2017 Chad Stein. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+    
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryTitle: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
