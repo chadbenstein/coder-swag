2ba89bb950e0f94be7ccd67d849b2131405fb002
Chad Stein <chadstein@Chads-MacBook-Pro.local>
2017-09-07 10:22:34 -0400

    Added product UI

---
+++ coder-swag/View/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Chad Stein on 9/7/17.
+//  Copyright © 2017 Chad Stein. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
